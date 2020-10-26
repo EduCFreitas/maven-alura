b0dd8f09457b9af216094445a7bf27fcf68a5340
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-10-26 11:06:47 -0300

    Aula 2: Integrando Maven ao Eclipse

--- produtos/src/main/java/br/com/alura/maven/Produto.java
+++ produtos/src/main/java/br/com/alura/maven/Produto.java
@@ -1,5 +1,15 @@
 package br.com.alura.maven;
 
-Public class Produto{
+public class Produto{
+	private final String nome;
+	private final double preco;
+	
+	
+	public Produto(String nome, double preco) {
+		super();
+		this.nome = nome;
+		this.preco = preco;
+	}
+	
 	
 }
